64be04cf1b099af4bef6209d1de1dbcf65df88b6
DannaP2309 <153132189+DannaP2309@users.noreply.github.com>
2023-12-06 16:16:18 -0500

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,81 @@
+# Fase-5---Accesibilidad-y-adaptaci-n-de-un-sitio-web-est-tico
+Fase 5 - Accesibilidad y adaptación de un sitio web estático
+< !DOCTYPE html>;
+ < html >;
+ < head >;
+ 
+     < meta charset; = "UTF-8" >
+  
+      < Book Magic D > Página de Libros Magic Books D </MAGIC BOOKS D>
+         <link rel = "stylesheet" type= "text/css" href= "estilos.css" >
+     </ head >
+     < body >
+     
+         < header >
+     
+             < h1 > Página de Libros</h1>
+        <nav>
+            <ul>
+                <li><a href = "#inicio" > Inicio </ a ></ li >
+     
+                     < li >< a href= "#autores" > Autores </ a ></ li >
+     
+                     < li >< a href= "#libros" > Libros </ a ></ li >
+     
+                     < li >< a href= "#resenas" > Reseñas </ a ></ li >
+     
+                     < li >< a href= "#relatos" > Relatos </ a ></ li >
+     
+                 </ ul >
+     
+             </ nav >
+     
+         </ header >
+     
+
+         < section id= "inicio" >
+     
+             < h2 > Inicio </ h2 >
+     
+             < !--"Un lector vive mil vidas antes de morir, el que no lee vive solo una" "(George R. R Martin)"
+    "Por medio de esta pagina se vera el gran mundo de la literatura, Encontraras tus libros favoritos, autoras de tu preferencia y sus debidas reseñas, las vuales te ayudaran a elegir tu proxima lectura"
+    "Que esperas entra a estre gran mundo y vive miles de vidas las cuales haran volar tu imagonacion "-->
+    </section>
+
+    <section id = "autores" >
+     
+             < h2 > Autores </ h2 >
+     
+             < !--"Alice Kellen - 5 Estrellas
+"Joana Marcus - 5 Estrellas
+"Flor M. Salvador - 5 Estrellas
+"Ariana Godoy - 5 Estrellas
+"Otros" -->
+    </section>
+
+    <section id = "libros" >
+     
+             < h2 > Libros </ h2 >
+     
+             < !--"Bilogia  Deja que ocurra Libro 1 - Todo lo que nunca Fuimos - Libro 2 - Todo lo que somos junto y muchos mas"-->
+    </section>
+
+    <section id = "resenas" >
+     
+             < h2 > Reseñas </ h2 >
+     
+             < !--"Boulevard de Flor M. Salvador Danna P. Es un libro fantastico, la verdad es de mis favoritos, " -->
+    </section>
+
+    <section id = "relatos" >
+     
+             < h2 > Relatos </ h2 >
+     
+             < !--"Descubre todos los relatos de tus autores favoritos aqui -->
+    </section>
+
+    <footer>
+        <p>Derechos de autor © 2023 Página de Libros</p>
+         </footer>
+</body>
+</html>;
