9a97f445a3bf2cbf5ce335fdc6c2fa50abb6af56
DannaP2309 <153132189+DannaP2309@users.noreply.github.com>
2023-12-06 16:19:26 -0500

    Create Style.cs

---
+++ Style.cs
@@ -0,0 +1,103 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fase_2
+{
+    internal class Style
+    {
+    MiSitioWeb
+    index.html
+    estilos.css
+
+    }
+   
+
+    body {
+  font-family: Arial, sans-serif;
+  background-color: #E89BB0;
+  margin: 0;
+  padding: 0;
+}
+
+.header
+{
+    background - color: ##FFB7EF;
+  color: ##FFB0AA;
+  padding: 20px;
+    text - align: center;
+}
+
+.container
+{
+    max - width: 960px;
+margin: 0 auto;
+padding: 20px;
+}
+
+h1
+{
+    font - size: 36px;
+color: #333;
+}
+
+p
+{
+    font - size: 18px;
+color: #666;
+}
+
+{
+}
+
+body
+{
+    font - family: 'Arial', sans - serif;
+margin: 0;
+padding: 0;
+}
+
+
+@media only screen and (max-width: 600px) {
+    body {
+        background - color: #FC9AB8;
+    }
+
+    
+}
+
+
+@media only screen and (min-width: 601px) and(max - width: 1024px) {
+    body {
+        background - color: #FC4E83;
+    }
+
+    
+}
+
+
+@media only screen and (min-width: 1025px) {
+    body {
+        background - color: ##C93E68;
+    }
+
+    
+}
+< !DOCTYPE html >
+ < html lang = "es" >
+  < head >
+  
+      < meta charset = "UTF-8" >
+   
+       < meta name = "viewport" content = "width=device-width, initial-scale=1.0" >
+      
+          < link rel = "stylesheet" href = "ruta/a/tu/style.css" >
+         
+             < title > Tu Sitio Web</title>
+            </head>
+<body>
+
+</body>
+</html>
